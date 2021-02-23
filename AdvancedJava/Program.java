15d12def68a26387205e4f01a340a9fcd9b9ecd4
SomberTM <50004094+SomberTM@users.noreply.github.com>
2021-02-22 19:43:25 -0500

    Lambdas Example

--- AdvancedJava/Program.java
+++ AdvancedJava/Program.java
@@ -1,15 +1,11 @@
 package AdvancedJava;
 
-public class Program {
+import AdvancedJava.Example.Testable;
 
+public class Program {
+    
     public static void main(String[] args) {
-        try {
-            Class<?> t = Class.forName("AdvancedJava.Lambdas.Example");
-            System.out.println(t);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        // Example.test(AdvancedJava.Lambdas.Example.class);
+        Example.test(Testable.LAMBDAS);
     }
 
 }

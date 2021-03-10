bf626f2442256b631a870d03f1743c19adfbce31
SomberTM <50004094+SomberTM@users.noreply.github.com>
2021-03-10 09:21:01 -0500

    Add files via upload

--- Assignments/3.7.4 Code/HorseBarnRunner.java
+++ Assignments/3.7.4 Code/HorseBarnRunner.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 /*
  * Activity 3.7.3
  */
@@ -42,6 +43,52 @@ public class HorseBarnRunner
         return lightest;
     }
 
+    public static int[] randomPermutation(int n) {
+        
+        // Construct a new array 'p' with length 'n'
+        int[] p = new int[n];
+        // Initialize 'p' with values from 1-n
+        for (int i = 0; i < n; i++) p[i] = i + 1;
+        
+        // Construct a new array 'r' with length 'n'
+        int[] r = new int[n];
+
+        do {
+            // Choose a random usable index from 0-n
+            int i = (int)(Math.random() * n);
+            
+            // Copy the value at index 'i' into 'c'
+            int c = p[i];
+
+            // Set the last non-used element of array 'r' to 'c'
+            r[n-1] = c;
+
+            // Set the value at 'i' to the last usable element of 'p'
+            p[i] = p[n-1];
+
+            // Decrement 'n'
+            n--;
+        } while (n > 0);
+
+        return r;
+    }
+
+    public static ArrayList<Horse> horsePermutator(ArrayList<Horse> p) {
+        
+        int n = p.size();
+        int[] positions = randomPermutation(n);
+
+        ArrayList<Horse> r = new ArrayList<Horse>(n);
+        for (int i = 0; i < n; i++)
+            r.add(null);
+
+        for (int i = 0; i < positions.length; i++) 
+            r.set(i, p.get(positions[i]-1));
+        
+        return r;
+    }
+
+
     public static void main(String[] args)
     {
         HorseBarn barn = new HorseBarn();
@@ -71,6 +118,6 @@ public class HorseBarnRunner
         }
 
         System.out.println(spaces);
-
+        System.out.println(horsePermutator(spaces));
     }
 } 
